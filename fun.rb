094081b9502d47b545b31526d8c8c2e38c2e9f1f
Lia Kitamura <kit4mura@gmail.com>
2014-05-22 15:33:36 -0600

    made changes to the question

--- fun.rb
+++ fun.rb
@@ -1,4 +1,7 @@
 puts "What is your first name?"
-name = gets.chomp 
-
+name = gets.chomp
 puts "Hello #{name}"
+
+puts "Ok, so what is your last name?"
+last = gets.chomp
+puts "Your name is #{name} #{last}"
